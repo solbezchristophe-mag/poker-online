2679ff5c97dc93955034c35259636439ba1c0a8a
solbezchristophe-mag <solbezchristophe@gmail.com>
2026-09-20 10:03:46 +0200

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -1,4 +1,4 @@
 // Après avoir déployé le serveur Node.js (par exemple sur Render),
 // colle son adresse publique entre les guillemets ci-dessous.
 // Exemple : window.POKER_SERVER_URL = "https://mon-poker.onrender.com";
-window.POKER_SERVER_URL = "";
+window.POKER_SERVER_URL = "https://poker-online-srva.onrender.com/";
